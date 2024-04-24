bd8b7caf1626206135d0802879887246427f2d1e
Garai <Garai.m@opendeusto.es>
2024-04-24 18:11:28 +0200

    migration made

--- DeustronicComponents/appDeustronicComponents/models.py
+++ DeustronicComponents/appDeustronicComponents/models.py
@@ -75,7 +75,7 @@ class PedidoProducto(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.IntegerField()
-    precio_total = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
