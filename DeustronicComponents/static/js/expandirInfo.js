ed9d9aa5c02c885eaabd47863c106d7aec7a5121
Oier Artaza <oier.artaza@opendeusto.es>
2024-05-24 11:44:56 +0200

    arreglado problema body duplicado y estilos

--- DeustronicComponents/static/js/expandirInfo.js
+++ DeustronicComponents/static/js/expandirInfo.js
@@ -3,19 +3,18 @@
 la página (mostrar/ocultar un bloque al hacer click en “expandir información”).
 */
 
-const botonExpandir = document.getElementById('boton-expandir');
-const contenedorExpansible = document.getElementsByClassName('contenedor-expansible')[0];
+const botonExpandir = document.getElementsByClassName('botonExpandir')[0];
+const contExp = document.getElementsByClassName('contenedorExpandible')[0];
 
 function mostrarOcultarBloque(estado) {
     if (estado === 'mostrar') {
-        contenedorExpansible.style.display = 'block';
+        contExp.style.display = 'block';
     } else if (estado === 'ocultar') {
-        contenedorExpansible.style.display = 'none';
+        contExp.style.display = 'none';
     }
 }
-//Lo cambia en el html el style, pero da igual. Hacer que empiece en none como dice el CSS.
 
 botonExpandir.addEventListener('click', () => {
-    const estadoActual = contenedorExpansible.style.display === 'none' ? 'mostrar' : 'ocultar';
+    const estadoActual = contExp.style.display === 'none' ? 'mostrar' : 'ocultar';
     mostrarOcultarBloque(estadoActual);
 });
